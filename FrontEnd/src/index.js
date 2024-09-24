42f83f3bb77460532c4506577a7c166f74b98feb
Gabriel <gabrielmonteiro1203@gmail.com>
2024-09-24 17:01:53 -0300

    feat: Tela Inicial e Dados

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Web from './TelaInicial/Web';
 import Login from './Login/login';
+import DadosCliente from './DadosCliente/dados';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,7 +18,12 @@ function App() {
 
   return (
     <React.StrictMode>
-      {isLoggedIn ? <Web /> : <Login onLogin={handleLogin} />}
+      <Router>
+        <Routes>
+          <Route path="/" element={isLoggedIn ? <Web /> : <Login onLogin={handleLogin} />} />
+          <Route path="/dados-cliente" element={<DadosCliente />} />
+        </Routes>
+      </Router>
     </React.StrictMode>
   );
 }
