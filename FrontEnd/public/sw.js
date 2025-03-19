c9f253e81e73105bbe2cf601816ba983eb2d1f74
Túlio C. Rocha <tulio.tcr2504@gmail.com>
2025-03-19 15:54:21 -0300

    Pacotes mudados, Resetar senha e Imagem da notificação

--- FrontEnd/public/sw.js
+++ FrontEnd/public/sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('push', function (event) {
-    const data = event.data ? event.data.json() : {};
-    const title = data.title || 'Nova Notificação';
-    const options = {
-      body: data.body || 'Você recebeu uma nova mensagem!',
-      icon: '/logo192.png', // Substitua pelo ícone do seu site
-      badge: '/badge.png',
-    };
-  
-    event.waitUntil(self.registration.showNotification(title, options));
-  });
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || 'Nova Notificação';
+  const options = {
+    body: data.body || 'Você recebeu uma nova mensagem!',
+    icon: '/dog.png', 
+    badge: '/badge.png',
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
   
   self.addEventListener('notificationclick', function (event) {
     event.notification.close();
